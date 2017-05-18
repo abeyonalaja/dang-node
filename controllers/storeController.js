8ca6656001d90bfabbf3fbf07efa95460ba24005
Abey Onalaja <abey.onalaja@gmail.com>
2017-05-18 23:17:36 +0100

    File upload

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,21 @@
 
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid')
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next){
+        const isPhoto = file.mimetype.startsWith('image/');
+        if(isPhoto) {
+            next(null, true);
+        } else {
+            next({message: 'that filetype isn\'t allowed!'}, false);
+        }
+    }
+};
 
 
 exports.homePage = (req, res) => {
@@ -9,6 +24,27 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add Store'});
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async(req, res, next) => {
+    if(!req.file){
+        next();
+        return;
+    }
+
+    // generate the file name with uuid
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+
+    // now resize the photo
+    const photo = await jimp.read(req.file.buffer)
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+
+    next();
+
 };
 
 exports.createStore = async (req,res) => {
@@ -31,6 +67,8 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+    // Set the location data to be a point
+    req.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
         new: true,
         runValidators: true
