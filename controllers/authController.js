5f9363e9fd25c1f45bea3a724acc1e61b71521da
Abey Onalaja <abey.onalaja@gmail.com>
2017-05-25 19:39:04 +0100

    Add virtual fields for user gravitar, and protect routes using middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,3 +7,21 @@ exports.login = passport.authenticate('local', {
     successRedirect: '/',
     successFlash: "You have logged in"
 });
+
+exports.logout = (req, res) => {
+    req.logOut();
+    req.flash('success', 'You are now logged out!');
+    res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+
+    if(req.isAuthenticated()){
+        next();
+        return;
+    }
+
+    req.flash('error', 'Oops you must be logged in to do that!');
+    res.redirect('/login');
+
+}
